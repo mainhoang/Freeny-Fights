53e43ac486b4631df6d027274c304d96ee50f9ce
Mai Hoang <mnhoang.80@gmail.com>
2017-02-23 18:05:51 -0800

    Add chooseYourPlayers & stageFight functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	console.log("hello");
+	// console.log("hello");
 	
 		var characters = [
 			kitty = {
@@ -43,26 +43,25 @@ $(document).ready(function(){
 		];
 
 		var enemy = "";
-		var me = "";
-		var winCounter = 0;
-		var lossCounter = 0;
+		var yourCharacter = {};
 		var characterInfo = "";
-		var meSelected = false;
-		var enemySelected = false;
+		var hasYourCharacter = false;
+		var hasEnemy = false;
 		var lockChar = false;
+		var gameStarted =false;
 
-		console.log(characters[0]);
 
-
-		function renderDisplay(){
+		function renderChoices(){
 
 			for(var i = 0; i < characters.length; i++){
-				
-				characterInfo = $("<div class='character col-lg-2 col-md-2 col-sm-4 col-xs-6'><h4 class='character-names'>" 
+
+				characterInfo = $("<div class='card clickable col-lg-2 col-md-2 col-sm-4 col-xs-6' value='" 
+								+ (i + 1) +
+								"'><h4 class='names'>" 
 								+ characters[i].name + 
 								"</h4><img class='character-images img-responsive' src='"
 								+ characters[i].imageURL +
-								"'><p class='character-points'>"
+								"'><p class='character-pts'>"
 								+ characters[i].health +
 								"</p></div>");
 
@@ -70,13 +69,69 @@ $(document).ready(function(){
 
 			}
 
-			$(".character").on("click", function(){
-			alert("yay!");
-		})
+		}
+
+		function stageFight(){
+
+			$(".before-start").css("display","none");
+			$(".started").css("display","block");
+
+			$(".your-player").text(yourCharacter.name);
+			$(".your-img").css("backgroundImage","url('" + yourCharacter.imageURL + "')");
+			$(".your-pts").text(yourCharacter.health);
+
+			$(".your-enemy").text(enemy.name);
+			$(".enemy-img").css("backgroundImage","url('" + enemy.imageURL + "')");
+			$(".enemy-pts").text(enemy.health);
 
 		}
 
-		renderDisplay();
+		function chooseYourPlayers(){
+
+			$(".card").on("click", function(){
+
+				if(hasEnemy === false){
+
+					$(this).css("display", "none");
+						
+					var characterValues = parseInt($(this).attr("value"));
+					var selectedCharacter = "";
+
+					for(var i = 0; i < characters.length; i++){
+
+						if(characterValues === i + 1){
+
+							selectedCharacter = characters[i];
+
+						}
+		
+					}
+
+					if(hasYourCharacter === false){
+
+						yourCharacter = selectedCharacter;
+						hasYourCharacter = true;
+
+					}else{
+
+						$(".card").removeClass("clickable");
+						enemy = selectedCharacter;
+						hasEnemy = true;
+						lockChar = true;
+
+					}
+
+					stageFight();
+				
+				}
+
+			})
+
+		}
+
+
+		renderChoices();
+		chooseYourPlayers();
 
 		
 
